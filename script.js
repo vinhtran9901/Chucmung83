7a5c34e39b70227d3e1896aedef37c2b13b3e7b0
vinhtran9901 <lovangay@gmail.com>
2025-03-07 22:53:26 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function showPage(pageId) {
+    document.querySelectorAll('.page').forEach(page => page.classList.remove('active'));
+    document.getElementById(pageId).classList.add('active');
+}
